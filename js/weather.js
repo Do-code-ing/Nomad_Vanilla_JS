de555711effb5c78723e0b691f3b6a56d4f16317
Do-code-ing <zxcvbn658@naver.com>
2021-08-08 02:35:59 +0900

    2021-08-08

--- js/weather.js
+++ js/weather.js
@@ -9,18 +9,22 @@ const API_KEY = "527bc277d5dc38c64055b1f238357ecf";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             weather.innerText = `${Math.round(data.main.temp)}℃ ${data.weather[0].description}`;
             city.innerText = data.name;
         });
 }
 
 function onGeoError() {
-    alert("Can't find you. No weather for you.")
+    weatherIcon.src = "img/no_weather.png";
+    weather.innerText = "Weather"
+    city.innerText = "Unknown"
+    alert("Can't find you. No weather for you.");
+
 }
 
 setInterval(navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError), 60000);
